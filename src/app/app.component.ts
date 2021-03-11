bb92a1d110e9e94c9fb5552364d220ba593b8f53
Rodas Zegeye <rodas.zegeye@equifax.com>
2021-03-11 10:51:33 -0500

    intial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'shoppingandrecipapp';
+
+loadedFeature = 'recipe';
+
+  onNavigation(feature: string){
+    this.loadedFeature = feature;
+
+
+  }
+}
